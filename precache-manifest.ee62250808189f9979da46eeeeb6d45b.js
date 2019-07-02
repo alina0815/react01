552c3d9d61842752f682712aa697de9fe8c6790c
Monika Antczak <mrokosik@man.poznan.pl>
2019-07-02 12:53:22 +0200

    Updates

---
+++ precache-manifest.ee62250808189f9979da46eeeeb6d45b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a742ec847b7094985091833e9e0f58",
+    "url": "/react01/index.html"
+  },
+  {
+    "revision": "507457630f316b7f02c0",
+    "url": "/react01/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react01/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "507457630f316b7f02c0",
+    "url": "/react01/static/js/main.3208cca5.chunk.js"
+  },
+  {
+    "revision": "10e1412781073ae93552",
+    "url": "/react01/static/js/runtime~main.4df7b55a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react01/static/media/logo.5d5d9eef.svg"
+  }
+]);
