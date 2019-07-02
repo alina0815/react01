beb93b8111aa3cf284ab81541256432e8022a0a0
Monika Antczak <mrokosik@man.poznan.pl>
2019-07-02 13:23:23 +0200

    Updates

--- precache-manifest.db218eaa0d116874c4553801ca52dfdf.js
+++ precache-manifest.db218eaa0d116874c4553801ca52dfdf.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d6a742ec847b7094985091833e9e0f58",
+    "revision": "cfc99738047f40defb0c665d3200bb26",
     "url": "/react01/index.html"
   },
   {
-    "revision": "507457630f316b7f02c0",
+    "revision": "cf3eab872d010e394ea0",
     "url": "/react01/static/css/main.2cce8147.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react01/static/js/2.b41502e9.chunk.js"
   },
   {
-    "revision": "507457630f316b7f02c0",
-    "url": "/react01/static/js/main.3208cca5.chunk.js"
+    "revision": "cf3eab872d010e394ea0",
+    "url": "/react01/static/js/main.a6de9a60.chunk.js"
   },
   {
     "revision": "10e1412781073ae93552",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react01/precache-manifest.ee62250808189f9979da46eeeeb6d45b.js"
+  "/react01/precache-manifest.db218eaa0d116874c4553801ca52dfdf.js"
 );
 
 self.addEventListener('message', (event) => {
