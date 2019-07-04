fa5b6392ba5026e46893b65a409d91ea8f429435
Monika Antczak <mrokosik@man.poznan.pl>
2019-07-04 17:41:14 +0200

    l

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world
+        </p>
+		<p>
+          hash: 0123456
         </p>
         <a
           className="App-link"
